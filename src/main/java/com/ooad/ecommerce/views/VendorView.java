48411e6a9286e71e6e5dac5b670efcf26f645465
Anuj Bhavani <anbh2038@colorado.edu>
2022-12-06 02:11:51 -0700

    Added support for adding new product

--- src/main/java/com/ooad/ecommerce/views/VendorView.java
+++ src/main/java/com/ooad/ecommerce/views/VendorView.java
@@ -3,6 +3,7 @@ package com.ooad.ecommerce.views;
 import com.ooad.ecommerce.controller.VendorController;
 import com.ooad.ecommerce.dto.ProductDto;
 import com.ooad.ecommerce.dto.UserInfoDto;
+import com.ooad.ecommerce.service.AuthService;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
@@ -27,11 +28,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 @PermitAll
 public class VendorView extends VerticalLayout {
 
-  public VendorView(@Autowired VendorController vendorController) {
+  public VendorView(VendorController vendorController, AuthService authService) {
     Button plusButton = new Button(new Icon(VaadinIcon.PLUS));
     plusButton.addThemeVariants(ButtonVariant.LUMO_ICON);
     plusButton.setText("Add Product");
-    List<ProductDto> products = vendorController.getMyProducts(1);
+    plusButton.addClickListener(e -> {
+      UI.getCurrent().navigate("new-product");
+    });
+    List<ProductDto> products = vendorController.getMyProducts(authService.getUserId());
     Div cards = new Div();
     cards.addClassName("row");
     for (ProductDto product : products) {
